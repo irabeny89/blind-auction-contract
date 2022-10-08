0a8b92fc76d7e83be2edb416e20429d6df3e5a1c
Ernest Irabor <irabeny89@gmail.com>
2022-10-08 21:37:13 +0100

    Refactor and add timeout function

--- utils/index.js
+++ utils/index.js
@@ -1,7 +1,18 @@
-module.exports = {
-  reduceString: stringValue => {
-    if (typeof stringValue === "string" && stringValue.length > 10)
-      return `${stringValue.substring(0, 5)}...${stringValue.slice(-3)}`
-    else throw new Error("Not string type and/or string less than 11")
-  }
+/**
+ * Reduce long strings greater than 10 characters and replace the excess in-between with ellipsis.
+ * @param stringValue to be reduced with ellipsis.
+ * @returns reduced string with ellipsis inbetween.
+ */
+function reduceString(stringValue) {
+  if (typeof stringValue === "string" && stringValue.length > 10)
+    return `${stringValue.substring(0, 5)}...${stringValue.slice(-3)}`
+  else throw new Error("Not string type and/or string less than 11")
 }
+
+/**
+ * Delays for a specified period of time in seconds.
+ * @param sTime delay period in seconds.
+ */
+const timeout = sTime => new Promise(res => setTimeout(res, sTime * 1e3))
+
+module.exports = { reduceString, timeout }
